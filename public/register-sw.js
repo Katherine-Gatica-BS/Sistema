31b18932c55ed4b93aa31310f48547d38cb3a237
Katherine-Gatica-BS <135108736+Katherine-Gatica-BS@users.noreply.github.com>
2026-08-15 13:00:21 -0400

    Delete public directory

--- public/register-sw.js
+++
@@ -1,9 +0,0 @@
-// Registrar Service Worker para PWA
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => console.log("SW registrado:", reg.scope))
-      .catch((err) => console.error("SW error:", err));
-  });
-}
